854050d7117d7d03f481c29c433d071fee01b30d
Andrés1221800 <andrescervantes1221@gmail.com>
2026-08-25 21:39:17 -0500

    agregar blueprints de producto,celular a app.py

--- Damilis-Software-main/app.py
+++ Damilis-Software-main/app.py
@@ -5,6 +5,8 @@ from config import Config
 from Routes.User_bp import us_bp
 from Routes.Admin_bp import adm_bp
 from Routes.Cliente_bp import cli_bp
+from Routes.Producto_bp import pro_bp
+from Routes.Telefono_bp import tel_bp
 
 
 app = Flask(__name__)
@@ -18,6 +20,8 @@ app.mysql = mysql
 app.register_blueprint(us_bp, url_prefix='/users')
 app.register_blueprint(adm_bp, url_prefix='/admins')
 app.register_blueprint(cli_bp, url_prefix='/clientes')
+app.register_blueprint(pro_bp, url_prefix='/productos')
+app.register_blueprint(tel_bp, url_prefix='/telefonos')
 
 
 if __name__ == '__main__':
